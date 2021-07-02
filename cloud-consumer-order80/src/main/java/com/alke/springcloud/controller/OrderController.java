cb7086e40542d37a882e84df31ab8f46747f5042
alke_glb <alke_glb@hotmail.com>
2021-07-02 19:54:15 +0800

    项目初步功能模块搭建

--- cloud-consumer-order80/src/main/java/com/alke/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/alke/springcloud/controller/OrderController.java
@@ -3,10 +3,7 @@ package com.alke.springcloud.controller;
 import com.alke.springcloud.entities.CommonResult;
 import com.alke.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
